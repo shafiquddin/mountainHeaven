d70a505224b4056389a40814a717f8935adbc369
shafiquddin <shafiquddin2k@gmail.com>
2023-04-13 16:10:27 +0530

    complete

--- js/script.js
+++ js/script.js
@@ -226,3 +226,47 @@ $(function () {
     }
   });
 });
+
+//Hamburger menu
+$(document).ready(function () {
+  /* Hamburger menu animation */
+  $(".open-button").click(function () {
+    $(this).toggleClass("open");
+  });
+
+  /* Menu fade/in out on mobile */
+  $(".open-button").click(function (e) {
+    e.preventDefault();
+    $(".nav-menu").toggleClass("open");
+  });
+});
+
+$(document).ready(function () {
+  $(".nav-menu a").click(function () {
+    $(".nav-menu").toggleClass("open");
+    $(".open-button").removeClass("open");
+  });
+});
+
+//Smooth scroll
+$(function () {
+  $("a[href*=\\#]:not([href=\\#])").click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html,body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000
+        );
+        return false;
+      }
+    }
+  });
+});
